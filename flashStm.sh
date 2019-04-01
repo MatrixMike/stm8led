eb1e896d19b48d10ed75a846331c36dda458a2f1
Mike H <matroxmike@icloud.com>
2019-04-01 16:14:43 +1100

    now works on Tecra

--- flashStm.sh
+++ flashStm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # flashStm.sh
-~/stm8flash/stm8flash -c stlink -p stm8s105c6 -w led.ihx
+# was : ~/stm8flash/stm8flash -c stlink -p stm8s105c6 -w led.ihx
 
-
+stm8flash -c stlink -p stm8s105c6 -w led.ihx
